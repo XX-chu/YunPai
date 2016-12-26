6c166379d51400d1b2fe3cf48844e4d939d14769
硕影 <shuoying@shuoyingdeimac.local>
2016-12-26 09:58:27 +0800

    Initial Commit

---
+++ ShuoYing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShuoYing
+//
+//  Created by 硕影 on 2016/12/26.
+//  Copyright © 2016年 硕影. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShuoYing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShuoYing
+//
+//  Created by 硕影 on 2016/12/26.
+//  Copyright © 2016年 硕影. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
